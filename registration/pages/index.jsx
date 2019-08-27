088a7fc07c4463e044d5acb149c5bc889930456c
shakedlokits <shaked.lokits@gmail.com>
2019-08-28 01:38:55 +0300

    Registration: Add Next.js registration client.

---
+++ registration/pages/index.jsx
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var react_1 = require("react");
+function Home() {
+    return <div>Welcome to Next.js!</div>;
+}
+exports.default = Home;
