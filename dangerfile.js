e4b5fbcff6e5f7b456eb733a8219706624f833b9
Shaked Lokits <shaked.lokits@gmail.com>
2020-01-05 23:07:27 +0200

    Danger: Set initial danger rules.

--- dangerfile.js
+++ dangerfile.js
@@ -1,4 +1,33 @@
-import {message, danger} from "danger"
+import {fail, warn, success, danger} from "danger"
 
-const modifiedMD = danger.git.modified_files.join("- ")
-message("Changed Files in this PR: \n - " + modifiedMD)
+// Global variables and constants
+let allSystemsCheck = true
+
+const body = danger.github.pr.body
+const title = danger.github.pr.title
+
+// Should mention an active issue
+const mentionsActiveIssue = !!body.match(/#(\d+)/)
+allSystemsCheck &= mentionsActiveIssue
+if (!mentionsActiveIssue) warn('Should mention active issue in the PR body, i.e #4')
+
+// Should have correct title format
+const titleFormat = /[A-Z]\w+: [A-z].*?\./
+const usesCorrectTitleFormat = !!title.match(titleFormat)
+allSystemsCheck &= usesCorrectTitleFormat
+if (!usesCorrectTitleFormat) warn(`Title format should follow 'Subject: What I changed.'`)
+
+// Should have at least 2 reviewers
+const reviews = danger.github.reviews
+const approvingReviewers = new Set(reviews.filter(r => r.state = 'APPROVED').map(r => r.user.id))
+const hasTwoApprovingReviews = approvingReviewers.size > 1
+allSystemsCheck &= hasTwoApprovingReviews
+if (!hasTwoApprovingReviews) warn('You need at least 2 reviews (including production reviewer)')
+
+// Should be reviewed by production reviewer
+const productionReviewers = ['shakedlokits']
+const approvedByProductionReviewer = reviews.filter(r => productionReviewers.includes(r.user.login)).length > 0
+allSystemsCheck &= approvedByProductionReviewer
+if (!approvedByProductionReviewer) fail('Merging requires a review by a production reviewer')
+
+if (allSystemsCheck) success('All systems check! ready to merge (:')
