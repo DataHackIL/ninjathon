370eeaaedae65f9cf0604abb5859bd8546a8be26
Shaked Lokits <shaked.lokits@gmail.com>
2020-01-05 21:20:37 +0200

    Backups: Added initial migrations.

---
+++ backups/migrations/1578244792823_create-users/up.sql
@@ -0,0 +1,17 @@
+CREATE TABLE public.users (
+    id integer NOT NULL,
+    email text NOT NULL,
+    password text NOT NULL,
+    role text DEFAULT 'participant'::text NOT NULL
+);
+CREATE SEQUENCE public.users_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+ALTER SEQUENCE public.users_id_seq OWNED BY public.users.id;
+ALTER TABLE ONLY public.users ALTER COLUMN id SET DEFAULT nextval('public.users_id_seq'::regclass);
+ALTER TABLE ONLY public.users
+    ADD CONSTRAINT users_pkey PRIMARY KEY (id);
