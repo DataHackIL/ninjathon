9fc49d63a90a91ff3aeb7e4a88befde1b604c9c7
Shaked Lokits <shaked.lokits@gmail.com>
2020-01-11 12:09:52 +0200

    Services: Refactore and separated services directory.

---
+++ services/mailer/index.ts
@@ -0,0 +1,32 @@
+import * as express from 'express'
+import * as bodyParser from 'body-parser'
+import { readFileSync } from 'fs'
+import { join } from 'path'
+import { graphqlExpress, graphiqlExpress } from 'apollo-server-express'
+import { makeExecutableSchema } from 'graphql-tools'
+
+import logger from './utilities/logger'
+import resolvers from './resolvers'
+
+const typeDefs = readFileSync(join(__dirname, './schema.graphql')).toString()
+const schema = makeExecutableSchema({ typeDefs, resolvers })
+
+const app = express()
+
+// TODO: should be replaced by https://github.com/hasura/graphql-engine/issues/2461
+const validateRequest = (req, res, next) => {
+    const isInvalidated: boolean = !req.headers['x-hasura-role'] || req.headers['x-hasura-role'] === 'anonymous'
+    const isIntrospection: boolean = req.body['operationName'] === 'IntrospectionQuery'
+    const isInternal: boolean = ['sendMail'].some(service => req.body['query'].includes(service))
+
+    if (isInvalidated && isInternal && !isIntrospection) {
+        res.status(403).json({
+            errors: ['Unauthorized access']
+        })
+    }
+    next()
+}
+
+app.use('/graphql', bodyParser.json(), validateRequest, graphqlExpress({ schema }))
+app.use('/graphiql', graphiqlExpress({ endpointURL: '/graphql' }))
+app.listen(3000, () => logger.info('Server running, try going to /graphiql', { port: 3000 }))
--- services/mailer/resolvers.ts
+++ services/mailer/resolvers.ts
@@ -1,4 +1,4 @@
-import logger from '../utilities/logger'
+import logger from './utilities/logger'
 
 import { createTransport } from 'nodemailer'
 
