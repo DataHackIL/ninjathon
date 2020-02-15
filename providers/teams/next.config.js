48e26a8ce097f7d8a41db299ead4e030cdbef0dc
wildeyes <xwildeyes@gmail.com>
2020-02-15 17:24:25 +0200

    Infra: Add support for absolute paths.

--- providers/teams/next.config.js
+++ providers/teams/next.config.js
@@ -1,3 +1,7 @@
 module.exports = {
   assetPrefix: '/' + require('./package.json').homepage,
+  webpack: config => {
+    config.resolve.alias['~'] = require('path').resolve(__dirname);
+    return config;
+  },
 }
