0c59e21ca75c9c3caa049a18a189c2300027dff3
wildeyes <xwildeyes@gmail.com>
2020-02-19 23:27:13 +0200

    Add apollo.ts.

---
+++ providers/teams/lib/apollo.ts
@@ -0,0 +1,26 @@
+import fetch from 'node-fetch'
+import { ApolloClient, InMemoryCache, NormalizedCacheObject, ApolloClientOptions } from 'apollo-boost'
+import { createHttpLink } from 'apollo-link-http';
+import cookie from 'js-cookie'
+
+const cache = new InMemoryCache({
+  addTypename: false
+})
+
+const token = cookie.get('token')
+const link = createHttpLink({
+  uri: 'http://localhost:2015/graphql',
+  headers: {
+    'Authorization': 'Bearer ' + token
+  },
+  fetch: fetch as any,
+})
+
+const options = {
+  fetch,
+  link,
+  cache,
+  ssrMode: !process.browser,
+} as ApolloClientOptions<NormalizedCacheObject>
+
+export const apolloClient = new ApolloClient<NormalizedCacheObject>(options)
