bd5cf0d8b6f2162a171e74ac14f6c02d76405b5e
wildeyes <xwildeyes@gmail.com>
2020-02-19 23:27:50 +0200

    Add team types.

---
+++ providers/teams/lib/common.ts
@@ -0,0 +1,20 @@
+export interface Team {
+  id: number
+  name: String
+  description: String
+}
+export interface Team_Challenges {
+    id: number
+    teamId: number // FK
+    challengeId: number // FK
+}
+export interface Team_Members {
+    id: number
+    teamId: number // FK
+    userId: number // FK
+}
+export interface Challenge {
+    id: number
+    description: String
+    createdByUserId: number // FK, Defines whether it's a team project (created by a user of the project) or sponsered challenge (created by a "user" of the sponser)
+}
